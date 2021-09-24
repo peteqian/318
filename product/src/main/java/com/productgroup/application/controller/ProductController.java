132565abf5c84b41dcb60985ee4e91791ad9b92d
Peter Qian <60642461+peteqian@users.noreply.github.com>
2021-09-24 11:35:31 +1000

    Refactored Product: Changed the service into Domain Service

--- product/src/main/java/com/productgroup/application/controller/ProductController.java
+++ product/src/main/java/com/productgroup/application/controller/ProductController.java
@@ -1,4 +1,4 @@
-package com.productgroup.controller;
+package com.productgroup.application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
---
+++ product/src/main/java/com/productgroup/application/service/ProductValidationService.java
@@ -0,0 +1,4 @@
+package com.productgroup.application.service;
+
+public class ProductValidationService {
+}
