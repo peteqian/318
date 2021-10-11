f40e793d8f1a682ab03103e6ea6ced6155c27cc8
Peter Qian <60642461+peteqian@users.noreply.github.com>
2021-10-11 15:20:07 +1100

    Update CustomerConfig.java

--- customer/src/main/java/com/customergroup/infrastructure/configuration/CustomerConfig.java
+++ customer/src/main/java/com/customergroup/infrastructure/configuration/CustomerConfig.java
@@ -21,36 +21,84 @@ public class CustomerConfig {
                                          ContactRespository contactRespository){
         return args -> {
             Customer apple = new Customer(
-                    "Apple",
+                    "CompanyApple",
                     "Somewhere in FreedomLand",
-                    "U.S.A"
-            );
+                    "U.S.A");
 
-            Customer tesla  = new Customer(
-                    "Tesla",
-                    "3500 Deer Creek Road " +
-                            "Palo Alto, CA 94304",
-                    "U.S.A"
-            );
-            customerRespository.save(apple);
-            customerRespository.save(tesla);
+            Customer bravo = new Customer("CompanyBravo",
+                    "88 Pitt St, Sydney, NSW, 2000",
+                    "Australia");
 
-            Contact peter = new Contact(
-                    "Peter",
+            Customer charlie = new Customer("CompanyCharlie",
+                    "Bit my finger St, Sydney, NSW 2000",
+                    "Australia");
+
+            Customer delta = new Customer("CompanyDelta",
+                    " 71 Bank St, North Sydney, NSW 2060",
+                    "Australia");
+
+            Customer echo = new Customer("CompanyEcho",
+                    "84 Pitt St, Sydney, NSW 2000",
+                    "Australia");
+
+            Customer epic = new Customer("Epic Games",
+                    "620 Crossroads Blvd., Cary, NC",
+                    "USA");
+
+            Customer tesla  = new Customer("Tesla",
+                    "3500 Deer Creek Road Palo Alto, CA 94304",
+                    "USA");
+
+            Customer valve = new Customer("Valve",
+                    "10400 Northeast Fourth Street Floor 14, Bellevue, WA 98004 USA",
+                    "USA");
+
+            Contact gabe = new Contact("Gabe Newell",
+                    "12-12-12",
+                    "valve@gmail.com",
+                    "CEO");
+
+            Contact peter = new Contact("Peter",
                     "+61-412-123-456",
                     "peter@gmail.com",
-                    "positionOne"
-            );
+                    "positionOne");
 
-            Contact pineapple = new Contact(
-                    "Pineapple",
+            Contact pineapple = new Contact("Pineapple",
                     "+61-412-123-456",
                     "pineapple@gmail.com",
-                    "positionTwo"
-            );
+                    "positionTwo");
 
+            Contact timSweeney = new Contact("Tim Sweeney",
+                    "123-123-123",
+                    "epic@apple.killer.com",
+                    "USA");
+
+            apple.setContact(peter);
+            peter.setCustomer(apple);
+
+            bravo.setContact(pineapple);
+            pineapple.setCustomer(bravo);
+
+            valve.setContact(gabe);
+            gabe.setCustomer(valve);
+
+            epic.setContact(timSweeney);
+            timSweeney.setCustomer(epic);
+
+            customerRespository.save(apple);
+            customerRespository.save(bravo);
+            customerRespository.save(charlie);
+            customerRespository.save(delta);
+            customerRespository.save(echo);
+            customerRespository.save(epic);
+            customerRespository.save(tesla);
+            customerRespository.save(valve);
+
+            contactRespository.save(gabe);
             contactRespository.save(peter);
             contactRespository.save(pineapple);
+            contactRespository.save(timSweeney);
+
         };
     }
 }
